cea39af941f505aa614325b0291b59d5cff9d2cf
Ali Asgar <alisabun007@gmail.com>
2021-10-09 12:19:56 +0000

    update

--- List all posts of a user.go
+++ List all posts of a user.go
@@ -3,3 +3,6 @@
 		Should be a GET request
 		URL should be ‘/posts/users/<Id here>'
 */
+
+// Request with fields[post] and fields[user] parameters:
+GET /post?include=author&fields[post]=title,body,author&fields[user]=name HTTP/2
