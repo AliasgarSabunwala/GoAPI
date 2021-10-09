81161b8d8ba9ceb8253d0e8e46d5b4bf1f83720c
Ali Asgar <alisabun007@gmail.com>
2021-10-09 07:04:25 +0000

    upd

--- HomePage.go
+++ HomePage.go
@@ -1,3 +1,14 @@
+/*
+<- Getting Started with A Basic API
+To get started we will have to create a very simple server which 
+can handle HTTP requests. To do this we’ll create a new file 
+called HomePage.go. Within this HomePage.go file we’ll want to 
+define 3 distinct functions. A homePage function that will handle 
+all requests to our root URL, a handleRequests function that will 
+match the URL path hit with a defined function and a main function 
+which will kick off our API.
+*/
+
 package main
 
 import (
