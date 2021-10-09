3a9c00540769223e57a0b4e96a2aac43dfff482c
Ali Asgar <alisabun007@gmail.com>
2021-10-09 12:02:17 +0000

    Code Updation

--- MongoDB.go
+++ MongoDB.go
@@ -1,7 +1,7 @@
 import "go.mongodb.org/mongo-driver/mongo"
 
 clientOptions := options.Client().
-    ApplyURI("mongodb+srv://alif:<password>@ag.znuaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    ApplyURI("mongodb+srv://alif:<vZJVWHXNtq5X-#c>@ag.znuaz.mongodb.net/AG?retryWrites=true&w=majority")
 ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 defer cancel()
 client, err := mongo.Connect(ctx, clientOptions)
