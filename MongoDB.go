542ff054ebe8f011926a93064d9e0fa1bc05bcf9
Ali Asgar <alisabun007@gmail.com>
2021-10-09 11:59:53 +0000

    Code Updatin

--- MongoDB.go
+++ MongoDB.go
@@ -1,23 +1,10 @@
-// To get started with the driver, import the mongo package, create a mongo.Client:
-import (
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    "go.mongodb.org/mongo-driver/mongo/readpref"
-)
+import "go.mongodb.org/mongo-driver/mongo"
 
-client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-
-
-// And connect it to your running MongoDB server
-// To do this in a single step, you can use the Connect function:
+clientOptions := options.Client().
+    ApplyURI("mongodb+srv://alif:<password>@ag.znuaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 defer cancel()
-client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-
-
-//Make sure to defer a call to Disconnect after instantiating your client:
-defer func() {
-    if err = client.Disconnect(ctx); err != nil {
-        panic(err)
-    }
-}()
+client, err := mongo.Connect(ctx, clientOptions)
+if err != nil {
+    log.Fatal(err)
+}
