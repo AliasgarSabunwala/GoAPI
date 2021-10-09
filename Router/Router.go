46829325dc2cff0aa42edfce8792deef23615962
Ali Asgar <alisabun007@gmail.com>
2021-10-09 10:18:47 +0000

    codes updation

--- Router/Router.go
+++ Router/Router.go
@@ -5,8 +5,19 @@ import "net/http"
 func main() http.Handler {
   mux := http.NewServeMux()
   mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("ok"))
+  if r.URL.Path != "/" || r.Method != http.MethodGet {
+    http.NotFound(w, r)
+    return
+  }
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte("ok"))
+})
   })
   return mux
 }
+
+s := http.Server{
+  Addr:    listenAddr,
+  Handler: router.NewRouter(),  // Our own instance of inbuilt servemux
+}
+
